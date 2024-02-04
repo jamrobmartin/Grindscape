2a56511d187a04b478895cf7babcd0e9645f6179
jamro <jamrobmartin@gmail.com>
2024-02-04 01:27:15 -0500

    Implemented basic structure of the Server.

---
+++ GrindscapeServer/UI/GrindscapeServerMainForm.cs
@@ -0,0 +1,36 @@
+﻿using GrindscapeServer.Controller;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrindscapeServer.UI
+{
+    public partial class GrindscapeServerMainForm : Form
+    {
+        public GrindscapeServerMainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void startServerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ServerMasterController.Instance.StartServer();
+        }
+
+        private void stopServerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ServerMasterController.Instance.StopServer();
+        }
+
+        private void GrindscapeServerMainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ServerMasterController.Instance.StopServer();
+        }
+    }
+}
